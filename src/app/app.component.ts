cd3b1af98b4c92eeaf247a30bebea311488cd72f
Alessandro Aprile <aprile.alessandro@gmail.com>
2018-02-18 19:21:14 +0100

    mock server started from packageJson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private http:HttpClient){
+    this.getAll()
+  }
+
+  getAll(){
+    this.http.get('http://localhost:3000/devices')
+      .subscribe(result=>console.log(result));
+  }
 }
